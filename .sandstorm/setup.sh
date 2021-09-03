15381d06a392794050b77ebac353805233c3b6bd
Troy J. Farrell <troy@entheossoft.com>
2021-09-03 01:14:35 -0500

    Install Cap'n Proto 0.9.0

--- .sandstorm/setup.sh
+++ .sandstorm/setup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-# When you change this file, you must take manual action. Read this doc:
-# - https://docs.sandstorm.io/en/latest/vagrant-spk/customizing/#setupsh
-
 set -euo pipefail
-# This is the ideal place to do things like:
-#
-#    export DEBIAN_FRONTEND=noninteractive
-#    apt-get update
-#    apt-get install -y nginx nodejs nodejs-legacy python2.7 mysql-server
-#
-# If the packages you're installing here need some configuration adjustments,
-# this is also a good place to do that:
-#
-#    sed --in-place='' \
-#            --expression 's/^user www-data/#user www-data/' \
-#            --expression 's#^pid /run/nginx.pid#pid /var/run/nginx.pid#' \
-#            --expression 's/^\s*error_log.*/error_log stderr;/' \
-#            --expression 's/^\s*access_log.*/access_log off;/' \
-#            /etc/nginx/nginx.conf
 
-# By default, this script does nothing.  You'll have to modify it as
-# appropriate for your application.
+source /opt/app/.sandstorm/environment
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update && apt-get upgrade
+
+# Install Cap'n Proto
+apt-get install --yes g++
+
+if [ ! -f "/usr/local/bin/capnpc" ]; then
+	$CURL -O "$CAPNPROTO_URL"
+	if [ ! "$(/usr/bin/sha256sum -c "$CAPNPROTO_SHA256")" ]; then
+		echo "SHA-256 check of $CAPNPROTO_TGZ failed."
+		exit 1
+	fi
+	tar zxf "$CAPNPROTO_TGZ"
+	cd "$CAPNPROTO_BASENAME"
+	./configure && make -j6 check && make install
+fi
+
 exit 0
