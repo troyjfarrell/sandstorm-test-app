f6f7a2940e424137068d21f028edd8e1b6ec5ce9
Troy J. Farrell <troy@entheossoft.com>
2021-09-03 19:44:27 -0500

    Install the Rust toolchain with Rustup

--- .sandstorm/setup.sh
+++ .sandstorm/setup.sh
@@ -8,6 +8,23 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update && apt-get upgrade
 
+# Install the Rust toolchain
+apt-get install --yes gcc-multilib
+
+if [ ! -f "$RUSTUP_INIT" ] || [ ! "$(/usr/bin/sha256sum -c "$RUSTUP_INIT_SHA256")" ]; then
+	$CURL -o "$RUSTUP_INIT" "$RUSTUP_INIT_URL"
+	if [ ! "$(/usr/bin/sha256sum -c "$RUSTUP_INIT_SHA256")" ]; then
+		echo "SHA-256 check of rustup-init failed."
+		exit 1
+	fi
+fi
+if [ ! -d "$RUSTUP_HOME" ] || [ ! -d "$CARGO_HOME" ]; then
+	chmod 755 "$RUSTUP_INIT"
+	RUSTUP_HOME="$RUSTUP_HOME" CARGO_HOME="$CARGO_HOME" $RUSTUP_INIT --default-host "$RUSTUP_INIT_HOST_TRIPLE" --default-toolchain stable --no-modify-path -y
+fi
+
+chown -R "$VAGRANT_USER":"$VAGRANT_GROUP" "$CARGO_HOME"
+
 # Install Cap'n Proto
 apt-get install --yes g++
 
